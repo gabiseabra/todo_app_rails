f1c59cea4770a6086d6f1b902f93f5ea9baddabe
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-27 05:40:32 -0200

    Added common entry

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,14 @@
 const webpack = require("webpack")
 const { environment } = require('@rails/webpacker')
 
+environment.entry.set("common", [
+  "babel-polyfill",
+  "react",
+  "react-dom",
+  "mobx",
+  "mobx-react"
+])
+
 environment.plugins.append(
   "common_chunks",
   new webpack.optimize.CommonsChunkPlugin({
