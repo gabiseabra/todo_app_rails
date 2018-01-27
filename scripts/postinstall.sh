0315c3e55ce081b492bc6583db342cddfec3ab9d
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-27 12:34:13 -0200

    Added temporary grommet fix

---
+++ scripts/postinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Grommet won't build on rails' asset pipeline.
+# This fix is neccessary until this PR is merged.
+# https://github.com/grommet/grommet/pull/1821
+
+BUGGY_FILE="$PWD/node_modules/grommet/scss/grommet-core/_objects.animate.scss"
+
+sed 's/^\&\./\./g' $BUGGY_FILE > $BUGGY_FILE
