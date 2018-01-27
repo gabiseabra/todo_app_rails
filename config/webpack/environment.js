93eb8dae478cfa2f0998c99bc9dcc57f5848f8b3
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-27 01:06:56 -0200

    Added webpack common chunks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
+const webpack = require("webpack")
 const { environment } = require('@rails/webpacker')
+
+console.log(environment.plugins)
+
+environment.plugins.append(
+  "common_chunks",
+  new webpack.optimize.CommonsChunkPlugin({
+    name: "common"
+  })
+)
 
 module.exports = environment
