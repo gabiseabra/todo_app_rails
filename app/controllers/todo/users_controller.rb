786220a1627591302928f339ba8a92fddf5fc6c8
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-27 01:26:10 -0200

    Added api controller

--- app/controllers/todo/users_controller.rb
+++ app/controllers/todo/users_controller.rb
@@ -1,5 +1,5 @@
-class Todo::UsersController < ApplicationController
-  before_action :set_todo_user, only: [:show, :edit, :update, :destroy]
+class Todo::UsersController < TodoController
+  before_action :set_todo_user, only: [:show, :update, :destroy]
 
   # GET /todo/users
   # GET /todo/users.json
@@ -12,42 +12,25 @@ class Todo::UsersController < ApplicationController
   def show
   end
 
-  # GET /todo/users/new
-  def new
-    @todo_user = Todo::User.new
-  end
-
-  # GET /todo/users/1/edit
-  def edit
-  end
-
   # POST /todo/users
   # POST /todo/users.json
   def create
     @todo_user = Todo::User.new(todo_user_params)
 
-    respond_to do |format|
-      if @todo_user.save
-        format.html { redirect_to @todo_user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @todo_user }
-      else
-        format.html { render :new }
-        format.json { render json: @todo_user.errors, status: :unprocessable_entity }
-      end
+    if @todo_user.save
+      render :show, status: :created, location: @todo_user
+    else
+      render json: @todo_user.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /todo/users/1
   # PATCH/PUT /todo/users/1.json
   def update
-    respond_to do |format|
-      if @todo_user.update(todo_user_params)
-        format.html { redirect_to @todo_user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @todo_user }
-      else
-        format.html { render :edit }
-        format.json { render json: @todo_user.errors, status: :unprocessable_entity }
-      end
+    if @todo_user.update(todo_user_params)
+      render :show, status: :ok, location: @todo_user
+    else
+      render json: @todo_user.errors, status: :unprocessable_entity
     end
   end
 
@@ -55,10 +38,6 @@ class Todo::UsersController < ApplicationController
   # DELETE /todo/users/1.json
   def destroy
     @todo_user.destroy
-    respond_to do |format|
-      format.html { redirect_to todo_users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
---
+++ app/controllers/todo_controller.rb
@@ -0,0 +1,2 @@
+class TodoController < ActionController::API
+end
