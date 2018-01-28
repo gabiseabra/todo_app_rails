d399b7499150cef4a49547c822b7dda9f120b8d6
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-28 19:03:45 -0200

    Disabled failure app redirect

--- lib/api_failure_app.rb
+++ lib/api_failure_app.rb
@@ -1,4 +1,19 @@
 class ApiFailureApp < Devise::FailureApp
+  def respond
+    if http_auth?
+      http_auth
+    elsif warden_options[:recall]
+      recall
+    else
+      http_auth_head
+    end
+  end
+
+  def http_auth_head
+    self.status = 401
+    self.headers['WWW-Authenticate'] = %(Basic realm=#{Devise.http_authentication_realm.inspect}) if http_auth_header?
+  end
+
   def http_auth_body
     return super unless request_format == :json
     { errors: { scope => [i18n_message] } }.to_json
