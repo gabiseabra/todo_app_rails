a6373102914a00b976326d8693668743839f1e35
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-29 02:38:04 -0200

    Fixed authentication tags helper content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def authentication_tags
-    tag.script(@current_todo_user.try(:authentication_token).to_json,
+    tag.script(@current_todo_user.try(:authentication_token).to_json.html_safe,
                id: 'authentication-token',
-               type: 'application/json') <<
-    tag.script(@current_todo_user.to_json,
+               type: 'application/json').html_safe <<
+    tag.script(@current_todo_user.to_json.html_safe,
                id: 'authenticated-user',
                type: 'application/json')
   end
