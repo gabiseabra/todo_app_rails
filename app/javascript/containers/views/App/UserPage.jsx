7be1b90d6375bb575733d8d371ed6bf285da0dc0
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-01 17:51:56 -0200

    Fixed UserPage

--- app/javascript/containers/views/App/UserPage.jsx
+++ app/javascript/containers/views/App/UserPage.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Page } from "@/components/views"
 import { Card } from "../../users"
-import { Card as TaskLists } from "../../task_lists"
+import { List as TaskLists } from "../../task_lists"
 
 export default function UserPage({ match }) {
   const { params: { id } } = match
