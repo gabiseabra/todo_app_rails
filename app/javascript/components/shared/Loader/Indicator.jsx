271fd082823a636dcffb6f1a1f2307eeac9e0fbe
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-02 07:43:02 -0200

    Fixed loader styles

--- app/javascript/components/shared/Loader/Indicator.jsx
+++ app/javascript/components/shared/Loader/Indicator.jsx
@@ -5,19 +5,22 @@ import Spinner from "grommet/components/icons/Spinning"
 export default class LoadingIndicator extends Component {
   get target() { return document.getElementById("app-header-indicator") }
 
-  renderChildren() {
-    const { loading, overlay } = this.props
-    return (
-      <div className="Shared-Loader--indicator" data-overlay={Boolean(overlay).toString()}>
-        <Spinner size={overlay ? "large" : "small"} />
-      </div>
+  renderMirror() {
+    return ReactDOM.createPortal(
+      <div className="Shared-Loader--mirror" />,
+      this.target
     )
   }
 
   render() {
-    const { overlay } = this.props
-    const children = this.renderChildren()
-    if(overlay) return children
-    else return ReactDOM.createPortal(children, this.target)
+    const { loading, overlay } = this.props
+    return (
+      <div
+        className="Shared-Loader--indicator"
+        data-overlay={Boolean(overlay).toString()}>
+        <Spinner size={overlay ? "large" : "small"} />
+        {!overlay && this.renderMirror()}
+      </div>
+    )
   }
 }
