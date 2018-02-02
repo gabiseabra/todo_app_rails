d7c267a0b19a7c9d06214db0a3b0c00bb2828f1b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-02 16:43:46 -0200

    Added api vary header

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,11 @@
 class TodoController < ActionController::API
   include ApiConcern
 
+  before_action :set_headers
+
   acts_as_token_authentication_handler_for Todo::User, as: :todo_user, except: %i[index show]
+
+  protected
 
   def resource_name
     :todo_user
@@ -13,5 +17,9 @@ class TodoController < ActionController::API
 
   def authenticate_todo_user
     head 401 unless is_current_todo_user?
+  end
+
+  def set_headers
+    response.headers['Vary'] = 'X-User-Token, X-User-Email'
   end
 end
