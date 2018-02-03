7e8d9c84b6c3abc85b0b94b0b308d860790b7871
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-02 23:22:50 -0200

    Fixed tasks_controller_spec#destroy

--- spec/controllers/todo/tasks_controller_spec.rb
+++ spec/controllers/todo/tasks_controller_spec.rb
@@ -111,7 +111,7 @@ RSpec.describe Todo::TasksController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    let(:subject) { user.task_lists.last }
+    let(:subject) { tasks.last }
     let(:request!) { delete :destroy, format: :json, params: { id: subject.to_param, **params }}
 
     authentication_context do
@@ -119,7 +119,7 @@ RSpec.describe Todo::TasksController, type: :controller do
         lambda do
           request!
           subject.reload
-        end.should change(subject.tasks, :count).by(-1)
+        end.should change(tasks_list.tasks, :count).by(-1)
       end
     end
   end
