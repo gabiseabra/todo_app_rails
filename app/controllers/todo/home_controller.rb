845092294f9df280b37b122bab0640cc1279a42e
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-05 18:18:10 -0200

    Changed /api resources order

--- app/controllers/todo/home_controller.rb
+++ app/controllers/todo/home_controller.rb
@@ -1,7 +1,9 @@
 class Todo::HomeController < TodoController
   # GET /api/index.json
   def index
-    @todo_task_lists = Todo::TaskList.visible.paginate(page: params.fetch(:page, 1))
+    @todo_task_lists = Todo::TaskList.visible
+    @todo_task_lists = @todo_task_lists.order(created_at: :desc)
+    @todo_task_lists = @todo_task_lists.paginate(page: params.fetch(:page, 1))
     render 'todo/task_lists/index'
   end
 end
